64a370211d8fc988a5c51de70dfc538c8ee1770c
tomzhang255 <tomzhang255@gmail.com>
2022-05-11 21:51:07 -0400

    expand

--- R/queries.R
+++ R/queries.R
@@ -6,11 +6,15 @@ world_ggplot2 <- ggplot2::map_data("world")
 
 # world map shape file
 # download.file("http://thematicmapping.org/downloads/TM_WORLD_BORDERS_SIMPL-0.3.zip", destfile="data/world_shape_file.zip")
+# world_spdf <- rgdal::readOGR(
+#   # dsn="/Users/tomzhang/_R/covidviz/R/data/",
+#                              dsn=base::paste0(base::getwd(),"/inst/extdata/"),
+#                       layer="TM_WORLD_BORDERS_SIMPL-0.3",
+#                       verbose=FALSE)
 world_spdf <- rgdal::readOGR(
-  # dsn="/Users/tomzhang/_R/covidviz/R/data/",
-                             dsn= base::paste0(base::getwd(),"/inst/extdata/"),
-                      layer="TM_WORLD_BORDERS_SIMPL-0.3",
-                      verbose=FALSE)
+  dsn=base::path.expand(base::paste0(base::getwd(),"/inst/extdata/")),
+  layer="TM_WORLD_BORDERS_SIMPL-0.3",
+  verbose=FALSE)
 
 # ==================== fix names ====================
 
